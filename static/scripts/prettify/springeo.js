7b3ace2a993a166c60824d4e77b3f276672215fc
Rémy Drouet <drouet@springeo.fr>
2016-04-13 17:45:03 +0200

    Search engine handle multi string

--- static/scripts/prettify/springeo.js
+++ static/scripts/prettify/springeo.js
@@ -1,15 +1,15 @@
 var $body = $('html');
 var searchMap = {};
 var currSearch = false;
-var location;
+var locationHref;
 
 function updateSearchMap(){
   $('nav a').each(function(index, elt){
     var href = $(elt).attr('href');
-    var rel = href.match(/([\w\.\#]+\.html[\w\.\#]*)(?!\/)/)[0].replace(".html","");
+    var rel = href.match(/([\w\.#]+\.html[\w\.#]*)(?!\/)/)[0].replace(".html","");
     rel = rel.split(/[\.#]/).filter(function (e) {return e.length>0});
     var key = rel[rel.length-1];
-    var method = /\#/.test(href) && rel.length>2?new RegExp(rel[0]):false;
+    var method = /#/.test(href) && rel.length>2?new RegExp(rel[0]):false;
     rel = rel.slice(0,rel.length-1);
     if(searchMap[key]){
       searchMap[key].links.push(href);
@@ -22,7 +22,7 @@ function updateSearchMap(){
       }
     }
 
-    for(relKey of rel){
+    for(var relKey of rel){
       if(searchMap[relKey]){
         searchMap[relKey].related.push(key);
       } else {
@@ -35,33 +35,52 @@ function updateSearchMap(){
   });
 }
 
-function search(word){
+function search(inSearch){
   var nbFound = 0;
   $('nav ul a').hide();
+  var words = decodeURI(inSearch).split(/[\s\.#]/);
+
   var keys = Object.keys(searchMap);
-  for (var key of keys) {
-    var reg = new RegExp(word,'i');
-    var regKey = new RegExp(key,'i');
-    if(reg.test(key)){
-      nbFound++
-      for(var link of searchMap[key].links){
-        var $elt = $('nav a[href="'+link+'"]');
-        $elt.show();
-        $elt.siblings('ul.methods').show();
-        // $elt.children('a').show();
-      }
-      for(var rel of searchMap[key].related){
-        for(var link of searchMap[rel].links){
-          if(!searchMap[key].method || searchMap[key].method.test(link)){
-            $elt = $('nav a[href="'+link+'"]');
+  
+  words.forEach(function (word, iWord) {
+    for (var key of keys) {
+      var reg = new RegExp(word, 'i');
+      var match = reg.test(key);
+      if(match) {
+        if (words.length > 1 && match) {
+          var bros = words.slice().filter(function (e) { return e.length>0; });
+          bros.splice(iWord, 1);
+          for (var i = 0; i < bros.length && match; i++) {
+            match = false;
+            for (var link of searchMap[key].links) {
+              var broReg = new RegExp(bros[i],'i');
+              if (match = broReg.test(link));
+                break;
+            }
+          }
+        }
+        if (match) {
+          nbFound++;
+          for (var link of searchMap[key].links) {
+            var $elt = $('nav a[href="' + link + '"]');
             $elt.show();
             $elt.siblings('ul.methods').show();
+            // $elt.children('a').show();
+          }
+          for (var rel of searchMap[key].related) {
+            for (var link of searchMap[rel].links) {
+              if (!searchMap[key].method || searchMap[key].method.test(link)) {
+                $elt = $('nav a[href="' + link + '"]');
+                $elt.show();
+                $elt.siblings('ul.methods').show();
+              }
+            }
           }
         }
       }
     }
-  }
-  return nbFound;
+    return nbFound;
+  });
 }
 
 var searchTime = 0;
@@ -88,7 +107,7 @@ function smoothLink(e){
 $(document).ready(function () {
     updateSearchMap();
 
-    currSearch = window.location.search.replace('?search=','');
+    currSearch = decodeURI(window.location.search.replace('?search=',''));
     locationHref = window.location.href.replace(window.location.search,'');
     if(currSearch && currSearch.length>0){
       $('#search').val(currSearch);
@@ -132,7 +151,9 @@ $(document).ready(function () {
     if(locationHref.match(/[\w\.]+.html/i)[0]!=href.match(/[\w\.]+.html/i)[0]){
       $res.siblings('ul.methods').hide(300);
       $('#main').fadeOut(400, function() {
-        window.location.href = href.replace(/#[\w\.]+/i,searchParam+selector);
+        if(selector)
+          window.location.href = href.replace(/#[\w\.]+/i,searchParam+selector);
+        else window.location.href = href+searchParam;
       });
     } else if(selector){
       //escape dot char
@@ -141,7 +162,7 @@ $(document).ready(function () {
         window.location.href = href.replace(/#[\w\.]+/i,searchParam+selector);
       });
     } else {
-      window.location.href = href.replace(/#[\w\.]+/i,searchParam+selector);
+      window.location.href = href+searchParam;
     }
   });
 });
